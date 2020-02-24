a86e8bfb9f7acb4cc0dfd687ecc13ebb36cac5b2
Megan <meganwork>
2020-02-24 16:12:28 -0500

    generating accounts and adding into vector

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <ctime>
 #include "bankAccount.h"
 #include "Randomizer.h"
 using namespace std;
 
 int main() {
-    //BankAccount(1000,)
+    time_t now = time(0);
+    srand(now);
+
+    for (int i = 0; i < 10; i ++) {
+        BankAccount account;
+        account.setBalance(rand() % 10000);
+        account.setInterestRate(rand() / RAND_MAX);
+        list.pushback(account)
+
+    }
+    //BankAccount(1000, .05);
+
     return 0;
 }
