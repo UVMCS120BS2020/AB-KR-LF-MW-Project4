4f3d533c3d6198970b068db4d848e5f31ec170e4
Megan <meganwork>
2020-02-24 16:32:05 -0500

    ++i not i++

--- main.cpp
+++ main.cpp
@@ -11,13 +11,16 @@ int main() {
     srand(now);
 
     //Generate vector of multiple accounts with appropriate fields
-    for (int i = 0; i < 10; i ++) {
+    for (int i = 0; i < 10; ++i) {
         BankAccount account;
         account.setID(i);
         account.setBalance(rand() % 10000);
         account.setInterestRate(rand() / RAND_MAX);
         randomizer.add(account);
     }
+
+
+
 
     return 0;
 }
