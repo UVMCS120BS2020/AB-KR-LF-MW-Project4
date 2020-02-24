b90e6305e50379405e2355bb91d7d76e2ca94821
Megan <meganwork>
2020-02-24 16:22:23 -0500

    added ID field into generator

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int main() {
     time_t now = time(0);
     srand(now);
 
+    //Generate vector of multiple accounts with appropriate fields
     for (int i = 0; i < 10; i ++) {
         BankAccount account;
+        account.setID(i);
         account.setBalance(rand() % 10000);
         account.setInterestRate(rand() / RAND_MAX);
         randomizer.add(account);
